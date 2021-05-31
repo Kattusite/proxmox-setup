8c2e2882b4549c4a492aea8e095b2942248ebe85
Kattusite <Kattusite@users.noreply.github.com>
2021-05-31 01:30:46 -0400

    Fix create_ubuntu_template.sh issues
    The original create_ubuntu_template.sh script had a number of syntax errors,
    typos, and assorted bugs and glitches. This commit removes as many of those
    issues as possible.
    
    (The script actually runs correctly when you invoke it now!)

--- create_ubuntu_template.sh
+++ create_ubuntu_template.sh
@@ -1,8 +1,9 @@
-#!/bin/bash +ex
+#!/bin/bash -ex
 #
 # Create an Ubuntu cloud-init template for use with Proxmox.
 #
 # Source:
+#    https://pve.proxmox.com/wiki/Cloud-Init_Support
 #    https://norocketscience.at/deploy-proxmox-virtual-machines-using-cloud-init/
 
 VMID=${1:-1804}
@@ -10,8 +11,8 @@ BUILD=${2:-focal}
 ISO_STORAGE=${3:-proxmox-iso}
 VM_STORAGE=${4:-vm-storage}
 
-if [[ -z "$VMID" ]] ; then
-    echo 'Usage: $0 <vmid> <focal,bionic,...> (<iso_storage> <vm_storage>)
+if [[ -z "$1" ]] ; then
+    echo "Usage: $0 <vmid> <focal,bionic,...> (<iso_storage> <vm_storage>)"
     exit
 fi
 
@@ -31,8 +32,11 @@ mv ${IMG}.img ${IMG}.qcow2
 # Import the disk image in the local Proxmox storage
 qm importdisk $VMID ${IMG}.qcow2 $VM_STORAGE
 
+# Clean up old disk image.
+rm ${IMG}.qcow2
+
 # Configure your virtual machine to use the uploaded image
-qm set $VMID --scsihw virtio-scsi-pci --scsi0 ${VM_STORAGE}:${VMID}/vm-${VMID}-disk-0.raw
+qm set $VMID --scsihw virtio-scsi-pci --scsi0 ${VM_STORAGE}:vm-${VMID}-disk-0
 
 # Adding the Cloud-init image as CD-Rom to your virtual machine
 qm set $VMID --ide2 $VM_STORAGE:cloudinit
@@ -46,8 +50,12 @@ qm set $VMID --serial0 socket --vga serial0
 # Finally create a template
 qm template $VMID
 
+set +ex
+
 # Create a virtual machine out of the template
-echo "To create a VM from this template, run:"
-echo "    $ qm clone $VMID <vm-id> --name <vm-name>"
-echo ""
-echo "Configure cloud-init settings via proxmox web UI, or `qm set`"
+echo
+echo 'To create a VM from this template, run:'
+echo ' $ qm clone $VMID <vm-id> --name <vm-name>'
+echo
+echo 'Configure cloud-init settings via proxmox web UI, or `qm set`'
+echo
